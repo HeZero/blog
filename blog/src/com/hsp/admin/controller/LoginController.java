902fe7c720741468cb8b836fe0305af5b352579a
Administrator <Administrator@PC-HSP>
2017-08-11 00:48:31 +0800

    MOD

--- blog/src/com/hsp/admin/controller/LoginController.java
+++ blog/src/com/hsp/admin/controller/LoginController.java
@@ -2,10 +2,9 @@ package com.hsp.admin.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
-import org.apache.shiro.authc.LockedAccountException;
-import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,10 +13,9 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.genesisdo.web.BaseController;
 import com.hsp.admin.pojo.User;
 import com.hsp.admin.service.IUserService;
-import com.hsp.core.HMap;
+import com.hsp.base.controller.BaseController;
 
 @Controller
 @RequestMapping(value="/admin/")
